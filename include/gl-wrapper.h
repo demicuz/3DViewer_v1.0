5b0aac0c7df7e377b2d39ada60af7f19a06a5d5f
Denis Mazurak <pure.demicube@gmail.com>
2023-01-07 01:10:37 +0300

    Refactor: move some GLFW stuff to a separate file

--- include/gl-wrapper.h
+++ include/gl-wrapper.h
@@ -1,9 +1,13 @@
 #ifndef GL_WRAPPER_H
 #define GL_WRAPPER_H
 
+#define GLFW_INCLUDE_NONE
 #include "glad/gl.h"
+#include <GLFW/glfw3.h>
+
 #include <stdbool.h>
 
+GLFWwindow *get_glfw_window(void);
 bool create_line_shader_program(GLuint *program);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -9,56 +9,6 @@
 #include <gl-wrapper.h>
 #include <vector.h>
 
-void key_callback(GLFWwindow *window, int key, int scancode, int action,
-                  int mode) {
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, GL_TRUE);
-  }
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  // TODO also update projection matrix, as it depends on the aspect ratio
-  glViewport(0, 0, width, height);
-}
-
-GLFWwindow *get_glfw_window(void) {
-  if (glfwInit() == GLFW_FALSE) {
-    (void)fprintf(stderr, "Failed to initialize GLFW\n");
-    exit(1);
-  }
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__ // TODO do I even need this?
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-  GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "3DViewer", NULL, NULL);
-  if (window == NULL) {
-    (void)fprintf(stderr, "Failed to create GLFW window\n");
-    glfwTerminate(); // TODO maybe not needed if completely shutting down
-    exit(1);
-  }
-  glfwMakeContextCurrent(window);
-
-  glfwSetKeyCallback(window, key_callback);
-
-  int version = gladLoadGL(glfwGetProcAddress);
-  if (version == 0) {
-    (void)fprintf(stderr, "Failed to initialize OpenGL context\n");
-    glfwTerminate(); // TODO maybe not needed if completely shutting down
-    exit(1);
-  }
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  // Successfully loaded OpenGL
-  printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version),
-         GLAD_VERSION_MINOR(version));
-
-  return window;
-}
-
 bool init_obj(t_object *obj) {
   mat4_set_identity(&obj->model);
 
